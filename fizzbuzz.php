a2987132ca373f096199ff0c60238518068d851e
Wyatt Barnes <wyattb413@gmail.com>
2016-08-31 10:06:50 -0500

    Completed Basic FizzBuzz Game

---
+++ fizzbuzz.php
@@ -0,0 +1,13 @@
+<?php
+
+for ($i = 1; $i <= 100; $i += 1) {
+	if ($i % 3 == 0) {
+		echo "fizz" . PHP_EOL;
+	} else if ($i % 5 == 0) {
+		echo "buzz" . PHP_EOL;
+	} else if ($i % 3 == 0 && $i % 5 == 0) {
+		echo "fizzbuzz" . PHP_EOL;
+	} else {
+		echo $i . PHP_EOL;
+	}
+}
