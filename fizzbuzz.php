02446b97bedf73a8bbef6d947a0cb3376547191a
Wyatt Barnes <wyattb413@gmail.com>
2016-08-31 10:33:42 -0500

    rearrenged order of if statements so that fizzbuzz would show up, as well as added charizard to say fizzbuzz

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,12 +1,13 @@
 <?php
 
 for ($i = 1; $i <= 100; $i += 1) {
-	if ($i % 3 == 0) {
+	if ($i % 3 == 0 && $i % 5 == 0) {
+		$fizzbuzz = shell_exec("cowsay -f charizardvice fizzbuzz!!");
+		echo $fizzbuzz;
+	} else if ($i % 3 == 0) {
 		echo "fizz" . PHP_EOL;
 	} else if ($i % 5 == 0) {
 		echo "buzz" . PHP_EOL;
-	} else if ($i % 3 == 0 && $i % 5 == 0) {
-		echo "fizzbuzz" . PHP_EOL;
 	} else {
 		echo $i . PHP_EOL;
 	}
